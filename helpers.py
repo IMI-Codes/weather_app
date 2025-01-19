8edac039f8428a0f3a4ec8a5a1c37c76b31e9c98
Manasseh <rougerapper@gmail.com>
2025-01-19 21:42:41 +0100

    got the current weather for the current location now to the entered location

---
+++ helpers.py
@@ -0,0 +1,2 @@
+API_KEY = "af99065f1b520a2248438ffc4deca409"
+BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-#using requests
-#pip install requests
+import requests
+import geocoder
+from helpers import BASE_URL, API_KEY
 
 
-#import request
-#r = requests.get(url)
-#r = requests.get(url/get,paramas=payload)
-#r.status_code
-#r.okay
-#r.url
-#r.json
+g = geocoder.ip("me")
+latitude, longitude = g.latlng
+current_lat = latitude
+current_long = longitude
+
+second_half = f"lat={latitude}&lon={longitude}&appid={API_KEY}"
+final_url = f"{BASE_URL}{second_half}"
+data = requests.get(final_url)
+print(data.json())
+
+# for entered location convert location to long and lat
